4cfce51f60263d3ff946da13d06fd9007057b1bc
Robert MacWha <trebor.ahwcam@gmail.com>
2024-09-25 17:08:41 -0400

    style: revise docstrs

--- registry-contracts/src/SafeHarborRegistry.sol
+++ registry-contracts/src/SafeHarborRegistry.sol
@@ -37,8 +37,9 @@ contract SafeHarborRegistry is AgreementValidatorV1 {
         emit SafeHarborAdoption(adopter, oldDetails, agreementAddress);
     }
 
-    /// @notice Get the agreement address for the adopter.  Recursively queries fallback registries.
+    /// @notice Get the agreement address for the adopter. Recursively queries fallback registries.
     /// @param adopter The adopter to query.
+    /// @return address The agreement address.
     function getAgreement(address adopter) external view returns (address) {
         address agreement = agreements[adopter];
 
