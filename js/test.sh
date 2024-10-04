2f33e7958ad0eb129a77a059f6440dbf8f029bb2
Robert MacWha <trebor.ahwcam@gmail.com>
2024-10-04 12:59:33 -0400

    ci: Update setup.sh to work as a test

--- js/test.sh
+++ js/test.sh
@@ -22,7 +22,10 @@ anvil_pid=$!
 sleep 2
 
 # Deploy the smart contract
+export REGISTRY_DEPLOYER_PRIVATE_KEY=0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80
 forge script script/SafeHarborRegistryDeploy.s.sol:SafeHarborRegistryDeploy --rpc-url http://localhost:8545 --broadcast
 
-# Wait for any process to finish (keep the script running)
-wait
+# execute the JS test
+cd ../js
+
+npm test
