bc4ea1dfaf092d916fb2b4b34c2223f81d83cb3e
Robert MacWha <trebor.ahwcam@gmail.com>
2025-03-21 16:50:08 -0400

    feat: create v2 registry

---
+++ registry-contracts/src/v2/AgreementV2.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "../v1/AgreementV1.sol";
+
+/// @notice Contract that contains the AgreementDetails that will be deployed by the Agreement Factory.
+contract AgreementV2 {
+    /// @notice The details of the agreement.
+    AgreementDetailsV1 private details;
+
+    /// @notice Constructor that sets the details of the agreement.
+    /// @param _details The details of the agreement.
+    constructor(AgreementDetailsV1 memory _details) {
+        details = _details;
+    }
+
+    function version() external pure returns (string memory) {
+        return _version;
+    }
+
+    /// @notice Function that returns the details of the agreement.
+    /// @dev You need a view function, else it won't convert storage to memory automatically for the nested structs.
+    /// @return AgreementDetailsV1 The details of the agreement.
+    function getDetails() external view returns (AgreementDetailsV1 memory) {
+        return details;
+    }
+}
---
+++ registry-contracts/src/v2/SafeHarborRegistry.sol
@@ -0,0 +1,61 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "../v1/AgreementValidatorV1.sol";
+import "./AgreementV2.sol" as V2;
+
+/// @title The Safe Harbor Registry. See www.securityalliance.org for details.
+contract SafeHarborRegistry is AgreementValidatorV1 {
+    /// @notice A mapping which records the agreement details for a given governance/admin address.
+    mapping(address entity => address details) private agreements;
+
+    /// @notice The fallback registry.
+    SafeHarborRegistry fallbackRegistry;
+
+    /// ----- EVENTS -----
+
+    /// @notice An event that records when an address either newly adopts the Safe Harbor, or alters its previous terms.
+    event SafeHarborAdoption(address indexed entity, address oldDetails, address newDetails);
+
+    /// ----- ERRORS -----
+    error NoAgreement();
+
+    /// ----- METHODS -----
+    /// @notice Sets the factory and fallback registry addresses
+    constructor(address _fallbackRegistry) {
+        fallbackRegistry = SafeHarborRegistry(_fallbackRegistry);
+    }
+
+    function version() external pure returns (string memory) {
+        return _version;
+    }
+
+    /// @notice Function that creates a new AgreementV1 contract and records it as an adoption by msg.sender.
+    /// @param details The details of the agreement.
+    function adoptSafeHarbor(AgreementDetailsV1 memory details) external {
+        AgreementV1 agreementDetails = new AgreementV1(details);
+        address agreementAddress = address(agreementDetails);
+        address adopter = msg.sender;
+
+        address oldDetails = agreements[adopter];
+        agreements[adopter] = agreementAddress;
+        emit SafeHarborAdoption(adopter, oldDetails, agreementAddress);
+    }
+
+    /// @notice Get the agreement address for the adopter. Recursively queries fallback registries.
+    /// @param adopter The adopter to query.
+    /// @return address The agreement address.
+    function getAgreement(address adopter) external view returns (address) {
+        address agreement = agreements[adopter];
+
+        if (agreement != address(0)) {
+            return agreement;
+        }
+
+        if (address(fallbackRegistry) != address(0)) {
+            return fallbackRegistry.getAgreement(adopter);
+        }
+
+        revert NoAgreement();
+    }
+}
