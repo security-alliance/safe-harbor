6ede95c78c8302423ca907f2527a62f7c1a4ff13
Robert MacWha <trebor.ahwcam@gmail.com>
2025-04-15 11:53:07 -0400

    tidy: simplify IRegistry

--- registry-contracts/src/common/IRegistry.sol
+++ registry-contracts/src/common/IRegistry.sol
@@ -6,7 +6,4 @@ interface IRegistry {
     /// @param adopter The adopter to query.
     /// @return address The agreement address.
     function getAgreement(address adopter) external view returns (address);
-    function version() external pure returns (string memory);
-    function chains(uint256 id) external pure returns (string memory);
-    function chainIDs(string memory chainName) external pure returns (uint256);
 }
