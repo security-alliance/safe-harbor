44edddad4791ea9f11b567d89b2e0053f8831f4c
Robert MacWha <trebor.ahwcam@gmail.com>
2025-08-19 11:01:38 -0400

    switch GetAgreementDetailsV2 to use agreement address

--- registry-contracts/script/v2/GetAgreementDetailsV2.s.sol
+++ registry-contracts/script/v2/GetAgreementDetailsV2.s.sol
@@ -8,19 +8,8 @@ import "../../src/v2/AgreementV2.sol";
 import {logAgreementDetails} from "../../test/v2/mock.sol";
 
 contract GetAgreementDetailsV2 is ScriptBase {
-    address constant REGISTRY_ADDRESS = 0x1eaCD100B0546E433fbf4d773109cAD482c34686;
-
     function run() public view {
-        address owner = vm.envAddress("AGREEMENT_OWNER");
-
-        SafeHarborRegistryV2 registry = SafeHarborRegistryV2(REGISTRY_ADDRESS);
-
-        address agreementAddress = registry.getAgreement(owner);
-        require(agreementAddress != address(0), "No agreement found for owner");
-
-        console.log("Agreement address for owner:");
-        console.logAddress(agreementAddress);
-
+        address agreementAddress = vm.envAddress("AGREEMENT_ADDRESS");
         AgreementV2 agreement = AgreementV2(agreementAddress);
         AgreementDetailsV2 memory details = agreement.getDetails();
         logAgreementDetails(details);
