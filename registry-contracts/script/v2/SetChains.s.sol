b54c3512f38d676f037925ed5e3ee2174e68e19b
Robert MacWha <trebor.ahwcam@gmail.com>
2026-01-18 22:32:45 -0500

    feat: add quickswap chains (#41)
    * feat: add quickswap chains
    
    * format: revert

--- registry-contracts/script/v2/SetChains.s.sol
+++ registry-contracts/script/v2/SetChains.s.sol
@@ -68,6 +68,12 @@ contract SetChains is Script {
         caip2ChainIds[49] = "eip155:1868"; // Soneium
         caip2ChainIds[50] = "eip155:295"; // Hedera
         caip2ChainIds[51] = "eip155:9745"; // Plasma
+        caip2ChainIds[52] = "eip155:13371"; // Immutable zkEVM
+        caip2ChainIds[53] = "eip155:5031"; // Somnia Mainnet
+        caip2ChainIds[54] = "eip155:2000"; // Dogechain Mainnet
+        caip2ChainIds[55] = "eip155:3776"; // Aster zkEVM
+        caip2ChainIds[56] = "eip155:196"; // X Layer Mainnet
+        caip2ChainIds[58] = "eip155:5888"; // MANTRACHAIN Mainnet
 
         vm.broadcast(deployerPrivateKey);
         registry.setValidChains(caip2ChainIds);
